d2d086a2646be2061f4d96c7e1bdfeccba2a501c
FliPPeDround <734243792@qq.com>
2022-03-07 22:09:04 +0800

    feat：完善模版

---
+++ templates/ts-template/src/CustomElement/index.ce.vue
@@ -0,0 +1,63 @@
+<script setup lang='ts'>
+import { ref } from 'vue'
+const isDark = ref(false)
+</script>
+
+<template>
+  <button @click="isDark = !isDark">
+    <span v-if="isDark">🌚</span>
+    <span v-else>🌞</span>
+  </button>
+</template>
+
+<style>
+:host {
+  --color: #fbbf24;
+  --bg-normal: #fAfAf9;
+  --bg-active: #f5f5f4;
+  --font-size: 24px;
+}
+
+:host([dark]) {
+  --color: #fef3c7;
+  --bg-normal: #262626;
+  --bg-active: #2d2d2d;
+}
+
+button {
+  background-color: var(--bg-normal);
+  border: none;
+  border-radius: .5rem;
+  color: var(--color);
+  cursor: pointer;
+  display: flex;
+  font-size: var(--font-size);
+  overflow: hidden;
+  padding: 0.4em;
+  transition: background-color 0.3s ease, color 0.3s cubic-bezier(0.64, 0, 0.78, 0);
+}
+
+button:hover,
+button:focus {
+  background-color: var(--bg-active);
+  outline: none;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 0.3s ease-out;
+}
+
+.slide-enter-from {
+  transform: translateY(-150%);
+}
+
+.slide-enter-to,
+.slide-leave-from {
+  transform: translateY(0);
+}
+
+.slide-leave-to {
+  transform: translateY(150%);
+}
+</style>
--- templates/ts-template/vite.config.ts
+++ templates/ts-template/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        isCustomElement: (tag: string[]) => tag.includes('-')
+      }
+    }
+  })],
+  build: {
+    target: 'esnext',
+    minify: 'terser',
+    lib: {
+      entry: 'src/CustomElement/index.ts',
+      formats: ['es', 'cjs', 'iife'],
+      name: 'CustomElement'
+    }
+  }
 })
