bb5b5a708f4283111225140fe9277ce835353e57
FliPPeDround <734243792@qq.com>
2022-03-10 23:18:56 +0800

    fix: 修复同名文件冲突

--- src/question/packageName.ts
+++ src/question/packageName.ts
@@ -1,8 +1,18 @@
+import path from 'path'
+import fs from 'fs'
+
 export default () => {
   return {
     type: "input",
     name: "ProjectName",
     default: 'custom-element',
-    message: "Project name:"
+    message: "Project name:",
+    validate(value: string) {
+      const cwd = process.cwd()
+      const targetPath = path.join(cwd, value)
+      const isTemplateExit = fs.existsSync(targetPath)
+      if (!isTemplateExit) {return true}
+      return `Target directory "${value}" is exist. please enter again and continue`
+    }
   }
 }
